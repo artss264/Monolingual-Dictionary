b67a85bfd1f3aa970d51946d529e97807f25b1c7
Ashutosh Ranjan <artss264@gmail.com>
2015-12-20 14:21:29 +0530

    code files added

---
+++ script_extract_nouns.py
@@ -0,0 +1,13 @@
+f = open("tagged")
+a = f.readline()
+b = a.split()
+dis=[]
+for i in b:
+    j=i.split("/")
+    if "NN" in j[1] and j[0] not in dis:
+        dis.append(j[0])
+        print j[0]
+
+
+
+
